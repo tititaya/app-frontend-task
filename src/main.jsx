3fd155fe5500d4749a536180f20472cdd8ce92a7
Dreams <ornelrony@et.esiea.fr>
2025-03-31 17:10:47 +0200

    🔥 Fix: remove broken CSS import from index.html

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 //import './index.css'
 import App from './App'
-// Dans main.jsx ou App.jsx
-import './components/index.css';
+// src/main.jsx
+import './index.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
